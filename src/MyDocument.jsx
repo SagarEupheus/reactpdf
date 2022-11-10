908a8358532742fd0e928f0e9175e71ebbe0f6a7
SagarEupheus <uiintern@eupheus.in>
2022-11-10 12:32:38 +0530

    updated

--- src/MyDocument.jsx
+++ src/MyDocument.jsx
@@ -1,215 +1,3 @@
-// // import logo from "../assets/img/logo.png";
-// import jsPDF from "jspdf";
-// // import { renderToString } from "react-dom/server";
-
-// const MyDocument = () => {
-//   const doc = new jsPDF();
-
-//   doc.setFontSize(13);
-//   doc.setFont("about", "bold");
-//   doc.text("Proficiency Learning Solutions Pvt Ltd ", 75,20);
-//   doc.text("Main Mathura Road A-12 2nd Floor,Mohan Cooperative", 75, 28);
-//   doc.text("Industrial Estate New Delhi DL 110044 India", 75, 33);
-//   doc.setFont("invoice", "bold");
-//   doc.setFontSize(15);
-
-//   // doc.table(20, 120, generateData(18), headers, { autoSize: true });
-//   // Table
-//   // header
-//   doc.setLineWidth(0.5);
-//   doc.text("CREDIT NOTE", 95, 56);
-//   doc.line(20, 50, 203, 50);
-//   doc.setLineWidth(0.1);
-//   doc.line(20, 60, 203, 60);
-//   doc.setLineWidth(0.3);
-//   doc.line(20, 180, 20, 50);
-//   doc.line(20, 120, 203, 120);
-//   doc.line(90, 180, 90, 60);
-//   doc.line(150, 165, 150, 60);
-//   doc.line(90, 75, 203, 75);
-//   doc.line(90, 90, 203, 90);
-//   doc.line(90, 105, 203, 105);
-//   doc.line(90, 135, 203, 135);
-//   doc.line(90, 150, 203, 150);
-//   doc.line(90, 165, 203, 165);
-//   doc.line(90, 180, 203, 180);
-//   doc.setLineWidth(0.1);
-//   doc.line(20, 180, 203, 180);
-
-//   doc.setFont("bill", "bold");
-//   doc.setFontSize(9);
-//   doc.text("Bill To :", 23, 65);
-//   doc.setFontSize(10);
-//   doc.text("MKK Enterprises :", 23, 72);
-//   doc.setFont("about", "normal");
-//   doc.setFontSize(8);
-//   doc.text("F-2/13 Ratiya Marg Sangam Vihar,MKK Enterprises, ", 23, 76);
-//   doc.text("New Delhi - 110080", 23, 84);
-//   doc.text("Delhi - INDIA", 23, 88);
-//   doc.setFont("about", "bold");
-//   doc.setFontSize(10);
-//   doc.text("GSTIN Number :", 23, 108);
-
-//   //
-//   doc.setFont("sales", "bold");
-//   doc.setFontSize(10);
-//   doc.text("Sales Credit Invoice No. ", 95, 65);
-//   doc.setFont("sales", "normal");
-//   doc.text("CN / CN/31581/21-22 ", 95, 71);
-//   //
-//   doc.setFont("challan", "bold");
-//   doc.setFontSize(10);
-//   doc.text("Challan No. ", 95, 80);
-
-//   //
-//   doc.setFont("challan", "normal");
-//   doc.setFontSize(10);
-//   //
-
-//   doc.setFont("order", "bold");
-//   doc.setFontSize(10);
-//   doc.text("Reference No.", 95, 95);
-//   doc.setFont("order", "normal");
-//   doc.text("RI/52941/20-21 ", 95, 101);
-
-//   //
-//   doc.setFont("order", "bold");
-//   doc.setFontSize(10);
-//   doc.text("Order No.", 95, 110);
-//   //
-//   doc.setFontSize(10);
-//   doc.text("Dispatch Doc No.", 95, 125);
-//   //
-//   doc.setFontSize(10);
-//   doc.text("Transporter Name", 95, 140);
-//   //
-//   doc.setFont("order", "normal");
-//   doc.text("By Hand Sumit Goyal", 95, 146);
-
-//   doc.setFont("order", "bold");
-//   doc.text("Bill of Ladding/LR-RR No.", 95, 155);
-//   //
-//   doc.setFont("order", "normal");
-//   doc.text("By Hand Sumit Goyal", 95, 162);
-//   //
-//   doc.setFont("order", "bold");
-//   doc.text("Terms of Delivery", 95, 172);
-
-// // ship to
-
-// doc.setFont("bill", "bold");
-// doc.setFontSize(9);
-// doc.text("Ship To :", 23, 125);
-// doc.setFontSize(10);
-// doc.text("MKK Enterprises :", 23, 130);
-// doc.setFont("about", "normal");
-// doc.setFontSize(8);
-// doc.text("F-2/13 Ratiya Marg Sangam Vihar New Delhi, ", 23, 135);
-// doc.text("MKK Enterprises, ", 23, 138);
-// doc.text("Delhi - 110080", 23, 150);
-// doc.text("Delhi - INDIA", 23, 154);
-
-// doc.setFont("bill", "bold");
-//   doc.text("State Code : 07 ", 23, 162);
-//   doc.text("State Name : DELHI ", 23, 168);
-//   doc.text("Mobile : 9582400777  ", 23, 174);
-
-//   // doc.setFontSize(7);
-//   // doc.text("Description of Goods/", 23, 125);
-//   // doc.text("Description of Goods/", 23, 125);
-//   // doc.text("HSN-SAC Code", 23, 128);
-//   // doc.text("Quantity", 55, 128);
-//   // doc.text("Unit Price", 75, 128);
-//   // doc.text("Disc %", 95, 128);
-//   // doc.text("Amount", 115, 128);
-//   // doc.text("CGST[INR]", 135, 125);
-//   // doc.text("Rate", 134, 130);
-//   // doc.text("Amount", 142, 130);
-//   // doc.text("SGST[INR]", 160, 125);
-//   // doc.text("Rate", 159, 130);
-//   // doc.text("Amount", 167, 130);
-//   // doc.text("IGST[INR]", 185, 125);
-//   // doc.text("Rate", 184, 130);
-//   // doc.text("Amount", 192, 130);
-//   //   date
-//   // doc.setFont("date", "normal");
-//   // for (let i = 0; i < 4; i++) {
-//   //   doc.text("New Radiant Literature", 23, 140 + i * 8);
-//   //   doc.text("Reader Book 1", 23, 143 + i * 8);
-//   // }
-//   // for (let i = 0; i < 4; i++) {
-//   //   doc.text("2.00", 57, 140 + i * 8);
-//   // }
-//   // for (let i = 0; i < 4; i++) {
-//   //   doc.text("100.00", 77, 140 + i * 8);
-//   // }
-//   // for (let i = 0; i < 4; i++) {
-//   //   doc.text("100.00", 97, 140 + i * 8);
-//   // }
-//   // for (let i = 0; i < 4; i++) {
-//   //   doc.text("100.00", 117, 140 + i * 8);
-//   // }
-//   //   CGST DATA
-//   // for (let i = 0; i < 2; i++) {
-//   //   for (let j = 0; j < 4; j++) {
-//   //     doc.text("0.00", 134 + i * 12, 140 + j * 8);
-//   //   }
-//   // }
-//   // //   SGST DATA
-//   // for (let i = 0; i < 2; i++) {
-//   //   for (let j = 0; j < 4; j++) {
-//   //     doc.text("0.00", 159 + i * 12, 140 + j * 8);
-//   //   }
-//   // }
-//   // // IGST DATA
-//   // for (let i = 0; i < 2; i++) {
-//   //   for (let j = 0; j < 4; j++) {
-//   //     doc.text("0.00", 184 + i * 12, 140 + j * 8);
-//   //   }
-//   // }
-//   // //   total
-//   // doc.setFont("Total", "bold");
-//   // doc.text("Total: ", 26, 193);
-//   // doc.text("8.00", 58, 193);
-//   // doc.text("400.00", 117, 193);
-//   // doc.text("0.00", 146, 193);
-//   // doc.text("0.00", 171, 193);
-//   // doc.text("0.00", 196, 193);
-
-//   // //   Footer
-//   // doc.setFontSize(8);
-//   // doc.text("Invoice Value (In Words)", 54, 200);
-//   // doc.text("Sub Total[INR]:    1280.00", 164, 200);
-//   // doc.text("INR One Thousand Two Hundred Eighty Only", 23, 210);
-//   // doc.text("Remark :- Test Allied", 23, 215);
-//   // doc.text("Invoice Total[INR]: 1280.00", 164, 210);
-//   // doc.text("Net Amount Due: 1280.00", 164, 215);
-//   // doc.text("Banking Information", 23, 230);
-//   // doc.setFont("info", "normal");
-//   // doc.text("Bank Name:", 23, 235);
-//   // doc.text("A/C No:", 23, 240);
-//   // doc.text("IFSC Code:", 23, 245);
-//   // doc.setFont("info", "bold");
-//   // doc.text("Terms And Condition", 23, 255);
-
-//   let width = doc.internal.pageSize.getWidth();
-//   let height = doc.internal.pageSize.getHeight();
-
-// //   doc.addImage(img, "PNG", 10, 0, 40, 12);
-//   const print = () => {
-//     window.open(doc.output("bloburl"), "_blank");
-//     // pdf.save("pdf");
-//   };
-
-//   return (
-//     <>
-//       <button onClick={print}>download</button>
-//     </>
-//   );
-// };
-
-// export default MyDocument;
-
 // import logo from "../assets/img/logo.png";
 import jsPDF from "jspdf";
 // import { renderToString } from "react-dom/server";
@@ -244,6 +32,9 @@ const MyDocument = () => {
   doc.line(90, 180, 203, 180);
   doc.setLineWidth(0.1);
   doc.line(20, 180, 203, 180);
+  // h
+  doc.setFont("line","bold")
+  doc.line(203, 180, 203, 50);
 
   doc.setFont("bill", "bold");
   doc.setFontSize(9);
@@ -263,7 +54,24 @@ const MyDocument = () => {
   doc.setFont("sales", "bold");
   doc.setFontSize(10);
   doc.text("Sales Credit Invoice No. ", 95, 65);
+  doc.text(" Dated ", 165, 65);
   doc.setFont("sales", "normal");
+  doc.text("14/12/2021 ", 165, 70);
+  doc.setFont("sales", "bold");
+  doc.text("Mode/Terms of Payment", 158, 80);
+  doc.text(" Reference Date", 160, 95);
+  doc.setFont("sales", "normal");
+  doc.text("25-Feb-2021", 160, 102);
+  doc.setFont("sales", "bold");
+  doc.text("Dated", 160, 110);
+  doc.text("Motor Vehicle No.", 160, 125);
+  doc.text("Number of Boxes", 160, 140);
+  doc.setFont("sales", "normal");
+  doc.text("loose", 160, 145);
+  doc.setFont("sales", "bold");
+  doc.text("GR Date", 160, 155);
+  doc.setFont("sales", "normal");
+  doc.text("09/12/2021", 160, 162);
   doc.text("CN / CN/31581/21-22 ", 95, 71);
   //
   doc.setFont("challan", "bold");
@@ -441,7 +249,6 @@ const MyDocument = () => {
   doc.text("0.00", 170, 40);
   doc.text("0.00", 185, 40);
   doc.text("0.00", 195, 40);
-
   // 2nd goods
   doc.setFont("Total", "normal");
   doc.text("2", 4, 51);
@@ -462,7 +269,6 @@ const MyDocument = () => {
   doc.text("0.00", 170, 51);
   doc.text("0.00", 185, 51);
   doc.text("0.00", 195, 51);
-  
   // 3rd goods
   doc.setFont("Total", "normal");
   doc.text("3", 4, 59);
@@ -483,7 +289,6 @@ const MyDocument = () => {
   doc.text("0.00", 170, 59);
   doc.text("0.00", 185, 59);
   doc.text("0.00", 195, 59);
-  
   // Amount Chargeable (In Words) :
   doc.setFontSize(10);
   doc.setFont("amtc", "bold");
@@ -496,7 +301,6 @@ const MyDocument = () => {
   doc.text("Rupees Only", 5, 130);
   doc.text("Taxable:", 147, 124);
   doc.text("Total : 56,056.00", 147, 140);
-
   // compbnkdtls
   doc.text("Company's Bank Details" , 5,155 );
   doc.text("Bank Name" , 5,160 );
